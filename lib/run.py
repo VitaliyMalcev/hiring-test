5d1622178b00320b3570f3a393e76b601c870ad8
VitaliyMalcev <vitaliymalcev@gmail.com>
2020-10-04 02:26:47 +0300

    fix

--- lib/run.py
+++ lib/run.py
@@ -4,31 +4,27 @@ import pandas as pd
 from keras.models import load_model
 from pymystem3 import Mystem
 import numpy as np
-
-
+from keras.preprocessing import sequence
+import pickle
 
 if __name__ == '__main__':
     
     test = pd.read_csv('/hiring-test-data/test.csv')
-    model = load_model('/model.h5') 
-    
-    for i in range(10):
-        test[str(i) +" count"] = test['description'].apply(lambda x: x.count(str(i)))
+    model = load_model('model.h5') #загружаем модель
+    with open('tokenizer.pickle', 'rb') as handle:  # загружаем обученный токенайзер
+        tok = pickle.load(handle)	
 
     m = Mystem()
     def lemmatize(text):
         return m.lemmatize(text)
-
-    test['description'] = test['description'].apply(lemmatize)
-
-    words = ['один', 'два', 'три', 'четыре', 'пять','шесть',"семь", 'восемь', 'девять', 'ноль']
-    for i in words:
-        test[str(i) +" count"] = test['description'].apply(lambda x: x.count(str(i)))
-
-
-    test_features = test.drop(['title','description','subcategory', 'category', 'price', 'region', 'city', 'datetime_submitted'],axis =1)
+    test['description'] = test['description'].apply(lemmatize) #лематизируем
 	
-    preds = model.predict(test_features)
+	
+    max_len = 350
+    test_sequences = tok.texts_to_sequences(test['description'])
+    test_sequences_matrix = sequence.pad_sequences(test_sequences,maxlen=max_len) #строим матрицу
+	
+    preds = model.predict(test_sequences_matrix)#предсказываем
     
     target_prediction = pd.DataFrame()
     target_prediction['index'] = range(test.shape[0])
